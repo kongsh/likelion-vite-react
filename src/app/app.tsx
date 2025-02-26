9ea3cd987540381278ad8587b2a95c91a4a08b44
kongsh <ksh080838@gmail.com>
2025-02-26 20:45:07 +0900

    [테스트] Husky 확인용 커밋

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react';
 import './app.css';
 
 function App(props: { children: ReactElement }) {
-  return <div className="app1">{props.children}</div>;
+  return <div className="app">{props.children}</div>;
 }
 
 export default App;
